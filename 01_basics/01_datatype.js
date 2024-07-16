2f0b397838f9d64b22e610a7890ac3706544aa16
Swikrit Shivhare <33127600+swikritshivhare@users.noreply.github.com>
2024-07-16 08:56:24 +0000

    variables and datatypes

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3);
+console.log("Swikrit");
+
+// Primitive
+
+// String                           
+// Number
+// Boolean
+// undefined
+// null
+// symbol
+ 
+// Non-primitive
+
+// Object
+
+let name = "Swikrit";
+let age = 18;
+let isLoggedIn =  true;
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 122345
+let accountEmail = "sk@sk.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState
+
+accountEmail = "Uk@uk.com";
+accountPassword = "101010";
+accountCity = "Bengaluru";
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
