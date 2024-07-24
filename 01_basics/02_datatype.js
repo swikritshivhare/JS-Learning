688bae85664057cc8b5224f5b2d503f09fa47686
Swikrit Shivhare <33127600+swikritshivhare@users.noreply.github.com>
2024-07-24 06:54:46 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// console.log(3+3);
+// console.log("Swikrit");
+
+// Primitive
+
+// String                           
+// Number
+// Boolean
+// undefined
+// null
+// symbol
+ 
+// Non-primitive
+
+// Object
+
+let name = "Swikrit";
+let age = 18;
+let isLoggedIn =  true;
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
